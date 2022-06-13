ad8a3bfe3e298fb87283762be5a98633a6fe3833
huangchengrui <huangchengrui@rongcloud.cn>
2022-06-14 03:18:58 +0800

    feat:修改

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-import { NativeModules } from 'react-native';
-
-const { RNActionsheet } = NativeModules;
-
-export default RNActionsheet;
+export default class RNActionsheet {
+    static sentAnalysisPage() {
+      console.log('终于走了')
+    }
+}
--- ios/RNActionsheet.podspec
+++ ios/RNActionsheet.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "RNActionsheet"
-  s.version      = "1.0.3"
+  s.version      = "1.0.4"
   s.summary      = "RNActionsheet"
   s.description  = <<-DESC
                   RNActionsheet
